eeac01930fe6a706629f6e8509192785f75e8b3c
Heramb Mishra <76465490+herambgvd@users.noreply.github.com>
2021-12-19 09:25:49 +0530

    Add files via upload
    Java Basic Concepts are available in this file

---
+++ Main.java
@@ -0,0 +1,179 @@
+package com.mpstme;
+
+import java.util.Scanner;
+
+import java.lang.Math;
+
+public class Main {
+
+//
+
+    public static void main(String[] args) {
+	// Single Line Comment.
+//Assignment:-
+//        1. Neon Number --> 9 -- 9*9 = 81 --> 8+1 = 9
+//        2. Armstrong Number --> 125 = 1^3 +2^3 +5 ^3 = 125
+//        3. ISBN Number -->   123456789 (10 digit)  = 9*1 + 8*2 + 7*3 + === SUm Value
+   /// Sum Value Divide 11 == Remainder is zero
+   // Initial number is ISBN
+
+//        int a = 10;
+//        int b = 30;
+
+//        System.out.println(Math.max(a, b));
+//        System.out.println(Math.min(a,b));
+//        double randomNmber = Math.random();
+//        System.out.println(randomNmber);
+
+
+        // Looping
+        //1. For Loop
+        //2. while loop
+        //3. do-while loop
+
+        //// For Loop ///
+//        for(int i = 0; i<=25; i++){
+//            // Statements
+//            System.out.println(i);
+//        }
+
+
+        //// While Loop // ---> Break and Continue
+//        int i = 0;
+//        while(i<=25){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        /// DO- While Loop///
+//        int i = 0;
+//        do{
+//            //statement
+//            System.out.println(i);
+//            i++;
+//        } while(i<=25);
+
+        /// Exception Handling (try-Catch/throws/final)
+//        int[] mks = {95,25,65};
+//        System.out.println(mks[4]);
+//        System.out.println("Heramb");
+
+//        try{
+//            System.out.println(mks[4]);
+//        } catch (Exception e){
+//            //statements
+//            System.out.println(e);
+//        }
+//        System.out.println("Heramb");
+
+        ///Methods / Functions
+//        A function is a block of code that performs a specific task
+        //Types of Function:
+        //1.Pre-Defined Functions
+        //2.User-Defined Functions
+//        riddhi();
+//        riddhi();
+//        riddhi();
+//        Scanner input1 = new Scanner(System.in);
+//        int a = input1.nextInt();
+//        int b = input1.nextInt();
+//        int a = 10;
+//        int b = 25;
+//        riddhi1(a, b);
+        /*
+        * 1. Looping
+        * 2. Methods
+        * 3. Exception Handling
+        * Test
+        * */
+        /// Input
+//        Scanner hm = new Scanner(System.in);
+////        int n = hm.nextInt();
+//        System.out.println("Enter your Name:");
+////        String name = hm.next();
+//        String namefull = hm.nextLine();
+//        System.out.println(namefull);
+
+        /// Math Library
+        //1. Max Functions
+//        int a = 10;
+//        int b = 25 ;
+//        System.out.println(Math.max(a,b));
+//        System.out.println(Math.min(a,b));
+        // Random (0-1) double
+//        int randomNumber = (int)(Math.random()*10);
+//        double rand = Math.random();
+//        System.out.println(randomNumber);
+
+        // Loopings
+       /*
+       * 1. For loop
+       * 2. While loop
+       * 3. do while
+       * */
+//        System.out.println("Hello Java MPSTME");
+//        hm();
+//        hm();
+//        hm();
+//        for(int i=1 ;i<=20 ; i++){
+//            //statements
+//            System.out.println(i);
+//        }
+//        int i = 0;
+//        while(i<=20){
+//            //statements
+//            System.out.println(i);
+//            i++;
+//        }
+//        int i =0;
+//        do {
+//            //statements
+//            System.out.println(i);
+//            i++;
+//        } while(i<=20);
+
+        // Exception Handling
+        // EH in java is a mechanism to handle runtime errors so that normal flow of the application
+        // can be maintained.
+//        int[] marks = {12,45,48};
+//        System.out.println(marks[5]);
+//        try{
+////            System.out.println(marks[5]);
+//            int num = 100/0;
+//            System.out.println(num);
+//        }catch (Exception mpstme){
+////            System.out.println("Index is Out of Bound:");
+//            System.out.println(mpstme);
+//        }
+//        int num = 100/0;
+
+//        Scanner name = new Scanner(System.in);
+//        int hm = name.nextInt();
+//        System.out.println(hm);
+
+
+        /// Methods // Functions
+        // A function is a block of code that performs a
+        // specific task.
+//        float a = 10;
+//        float b = 20;
+//        mysum(a,b);
+    }
+    // Functions Type:
+    // 1. Pre-defined  --> 1. Argument(Parameter) Based
+    // 2. User defined      2. Non-argument(Parameter)
+//    public static void hm(){
+//        System.out.println("Hello Java MPSTME -2 ");
+//    }
+//    public static void mysum(float a, float b){
+//        float sum = a +b;
+//        System.out.println(sum);
+//    }
+//    public static void riddhi(int a , int b){
+//////        System.out.println("I don't love Java");
+////        int sum = a + b;
+////        System.out.println(sum);
+////
+////    }
+}
+
